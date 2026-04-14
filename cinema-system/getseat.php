334588875cb9c17b21341dc11ee52cf03819f7a7
jqmartinez <jqmartinez.student@asiancollege.edu.ph>
2026-04-14 08:51:08 +0800

    Delete cinema-system directory

--- cinema-system/getseat.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-$result = $conn->query("SELECT * FROM seats");
-$seats = [];
-while($row = $result->fetch_assoc()) {
-    $seats[] = $row;
-}
-echo json_encode($seats);
-?>
